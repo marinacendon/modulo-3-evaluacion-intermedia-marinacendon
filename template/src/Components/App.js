a4d4ce59fbcd66175ffd1097770f768cf05e9e5f
marinacendon <cendonmarina@gmail.com>
2020-11-12 09:23:18 +0100

    initial commit

---
+++ template/src/Components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import List from '../Data/List.json';
+
+import '../Stylesheet/App.css';
+
+function App() {
+  console.log(List);
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <PokeList></PokeList>
+        <Pokemon></Pokemon> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ template/src/Components/PokeList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PokeList(){
+
+
+
+
+render()
+
+
+}
+
+export default PokeList,
